2cc387299919d37ec7568885cb668cd231329eef
Marcio Mazza <marciomazza@gmail.com>
2020-12-03 15:50:24 -0300

    Fix python install and add python 3.9

--- config.py
+++ config.py
@@ -103,12 +103,16 @@ apt_install(
 if "pt_BR.utf8" not in run("locale -a", capture_output=True).stdout.splitlines():
     run("sudo locale-gen pt_BR.UTF-8")
 
-# install python versions 3.6 to 3.8
-python_versions = " ".join(f"python3.{v} python3.{v}-dev" for v in [6, 7, 8])
-if any(is_not_dpkg_installed(p) for p in python_versions.split()):
+# install python versions 3.6 to 3.9
+python_versions = [6, 7, 8, 9]
+python_packages = " ".join(f"python3.{v} python3.{v}-dev" for v in python_versions)
+if any(is_not_dpkg_installed(p) for p in python_packages.split()):
     run("sudo add-apt-repository ppa:deadsnakes/ppa --yes")
     run("sudo apt-get update")
-    apt_install("nodejs")
+    apt_install(python_packages)
+    # setup alternatives and make python3 point to the lastest
+    for version in python_versions:
+        run("sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.{version} {version}")
 
 pip_install("virtualenvwrapper")
 
