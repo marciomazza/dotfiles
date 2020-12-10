4681efbf14f9ce2f88e55719a6694ed515d78c55
Marcio Mazza <marciomazza@gmail.com>
2020-12-10 18:24:05 -0300

    Add ipython extension ipython-autoimport pprintpp

--- files/home/.ipython/profile_default/ipython_config.py
+++ files/home/.ipython/profile_default/ipython_config.py
@@ -2,5 +2,16 @@ c = get_config()  # noqa
 c.InteractiveShell.autocall = 2
 c.InteractiveShell.logstart = True
 c.InteractiveShellApp.extensions = ["autoreload"]
-c.InteractiveShellApp.exec_lines = ["%autoreload 2"]
 c.PlainTextFormatter.max_width = 110
+c.InteractiveShellApp.exec_lines = [
+    "%autoreload 2",
+    # load extensions ignoring absent ones
+    """
+for ext in ["ipython_autoimport", "pprintpp"]:
+    try:
+        get_ipython().run_line_magic("load_ext", ext)
+    except ImportError: pass
+""",
+]
+# c.TerminalInteractiveShell.editing_mode = "vi"
+c.TerminalInteractiveShell.autoformatter = "black"
