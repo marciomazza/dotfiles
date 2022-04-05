89af70f8f991cee5c445b22562651a96ff0b84d8
Marcio Mazza <marciomazza@gmail.com>
2022-04-05 13:02:25 -0300

    Simplify python install (after ubuntu 22.04)

--- config.py
+++ config.py
@@ -70,19 +70,11 @@ for here in files_home_dir.rglob("*"):
 # <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 # python
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-python_versions = [6, 7, 8, 9, 10]
+python_versions = [9, 10]
 python_packages = " ".join(f"python3.{v} python3.{v}-dev" for v in python_versions)
 if any(is_not_dpkg_installed(p) for p in python_packages.split()):
-    run("sudo add-apt-repository ppa:deadsnakes/ppa --yes")
     run("sudo apt-get update")
     apt_install(python_packages)
-    # setup alternatives
-    for version in python_versions:
-        run(
-            "sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.{version} {version}"
-        )
-    # make python3 point to python3.8 (python3.9 breaks terminator)
-    run("sudo update-alternatives  --set python3 /usr/bin/python3.8")
 
 pip_install("virtualenvwrapper")
 
