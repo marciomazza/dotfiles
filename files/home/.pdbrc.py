1d414c9f94610c3627c927e612de53fc70c89ed2
Marcio Mazza <marciomazza@gmail.com>
2022-09-06 17:50:23 -0300

    Save pdb history across sessions

--- files/home/.pdbrc.py
+++ files/home/.pdbrc.py
@@ -1,3 +1,25 @@
 import pdb
+
+
 class Config(pdb.DefaultConfig):
     sticky_by_default = True
+
+
+def _pdbrc_init():
+    # Save history across sessions
+    import readline
+    from pathlib import Path
+
+    histfile = Path("~/.pdb-pyhist").expanduser()
+    try:
+        readline.read_history_file(histfile)
+    except IOError:
+        pass
+    import atexit
+
+    atexit.register(readline.write_history_file, histfile)
+    readline.set_history_length(1000)
+
+
+_pdbrc_init()
+del _pdbrc_init
