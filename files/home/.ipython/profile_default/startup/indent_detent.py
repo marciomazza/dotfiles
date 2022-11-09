ce4eba1f672b329cacdf74d531a09da613791660
Marcio Mazza <marciomazza@gmail.com>
2022-11-09 09:07:45 -0300

    Restrict IPython startup customization to recent versions

--- files/home/.ipython/profile_default/startup/indent_detent.py
+++ files/home/.ipython/profile_default/startup/indent_detent.py
@@ -1,21 +1,20 @@
+import IPython
 from IPython import get_ipython
 from prompt_toolkit.buffer import indent, unindent
 
-ip = get_ipython()
-kb = ip.pt_app.key_bindings
+if int(IPython.__version__.split(".")[0]) > 5:
+    ip = get_ipython()
+    kb = ip.pt_app.key_bindings
 
+    def _register(func, key):
+        @kb.add("escape", key)
+        def _indent_unindent(event):
+            buffer = event.current_buffer
+            func(buffer, 0, buffer.document.line_count)
 
-def _register(func, key):
-    @kb.add("escape", key)
-    def _indent_unindent(event):
-        buffer = event.current_buffer
-        func(buffer, 0, buffer.document.line_count)
+    def _do_register():
+        for func, key in ((indent, ">"), (unindent, "<")):
+            _register(func, key)
 
-
-def _do_register():
-    for func, key in ((indent, ">"), (unindent, "<")):
-        _register(func, key)
-
-
-# run like this to avoid binding variables here
-_do_register()
+    # run like this to avoid binding variables here
+    _do_register()
