70db9d7cefa40a73e5e2c7615f289c483fa94909
Marcio Mazza <marciomazza@gmail.com>
2025-08-07 08:19:12 -0300

    update spotify install

--- .z/install/spotify.sh
+++ .z/install/spotify.sh
@@ -1,10 +1,6 @@
 # https://www.spotify.com/us/download/linux/
 
-# You will first need to configure our debian repository:
-
-curl -sS https://download.spotify.com/debian/pubkey_6224F9941A8AA6D1.gpg | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
-echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
-
-# Then you can install the Spotify client:
+curl -sS https://download.spotify.com/debian/pubkey_C85668DF69375001.gpg | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
+echo "deb https://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
 
 sudo apt-get update && sudo apt-get install spotify-client
