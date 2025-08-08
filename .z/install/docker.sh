021fbaeb939dbfa9c15016c6c340a89633f63b76
Marcio Mazza <marciomazza@gmail.com>
2025-08-08 17:45:52 -0300

    make docker install silent

--- .z/install/docker.sh
+++ .z/install/docker.sh
@@ -1,7 +1,7 @@
 # https://docs.docker.com/engine/install/ubuntu/
 
 # Add Docker's official GPG key:
-sudo apt-get install ca-certificates curl
+sudo apt install --yes --quiet ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
@@ -11,9 +11,9 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
+sudo apt update
 
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt install --yes --quiet docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 # https://docs.docker.com/engine/install/linux-postinstall/
 sudo usermod -aG docker $USER
