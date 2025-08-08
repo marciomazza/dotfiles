e8b12aad5afbcb86d5068eca618dc043d624cf52
Marcio Mazza <marciomazza@gmail.com>
2025-08-08 10:27:42 -0300

    clone separate neovim config

--- .z/bootstrap.sh
+++ .z/bootstrap.sh
@@ -3,3 +3,7 @@ git clone --bare git@github.com:marciomazza/dotfiles.git ~/.z/.dotfiles.git
 export GIT_DIR=/home/mazza/.z/.dotfiles.git GIT_WORK_TREE=/home/mazza
 git config --local include.path ../.gitconfig
 git checkout -f
+./config.py
+if [ ! -d ~/.config/nvim ]; then
+    git clone git@github.com:marciomazza/neovim_config.git ~/.config/nvim
+fi
