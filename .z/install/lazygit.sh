92a14dc9433af1c0ad46a1756f90ddf0ea2daffb
Marcio Mazza <marciomazza@gmail.com>
2025-08-08 12:31:35 -0300

    Install lazygit

---
+++ .z/install/lazygit.sh
@@ -0,0 +1,7 @@
+# https://github.com/jesseduffield/lazygit?tab=readme-ov-file#debian-and-ubuntu 
+# XXX: keep this only until 26.04 LTS
+
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+sudo install lazygit -D -t /usr/local/bin/
