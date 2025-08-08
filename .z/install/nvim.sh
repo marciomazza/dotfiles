28fc75a144df8fda6dc0e311eee3508d43a9ff2c
Marcio Mazza <marciomazza@gmail.com>
2025-08-08 12:21:36 -0300

    Fix nvim install

--- .z/install/nvim.sh
+++ .z/install/nvim.sh
@@ -1,8 +1,8 @@
 # https://github.com/neovim/neovim/blob/master/INSTALL.md#linux
 
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
 sudo rm -rf /opt/nvim
-sudo tar -C /opt -xzf nvim-linux64.tar.gz
+sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
 
 # for preservim/tagbar
 sudo apt install universal-ctags
