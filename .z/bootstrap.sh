41ef753b9006fc617d8e53799f02475497238af1
Marcio Mazza <marciomazza@gmail.com>
2025-08-16 19:11:09 -0300

    Refactor

--- .z/bootstrap.sh
+++ .z/bootstrap.sh
@@ -1,9 +1,10 @@
-mkdir -p ~/.z
-git clone --bare git@github.com:marciomazza/dotfiles.git ~/.z/.dotfiles.git
-export GIT_DIR=/home/mazza/.z/.dotfiles.git GIT_WORK_TREE=/home/mazza
-git config --local include.path ../.gitconfig
+BASE_DIR=$HOME/.z
+mkdir -p $BASE_DIR
+git clone --bare git@github.com:marciomazza/dotfiles.git $BASE_DIR/.dotfiles.git
+export GIT_DIR=$BASE_DIR/.dotfiles.git GIT_WORK_TREE=$HOME
 git checkout -f
+git config --local include.path ../.gitconfig
 ./config.py
-if [ ! -d ~/.config/nvim ]; then
-    git clone git@github.com:marciomazza/neovim_config.git ~/.config/nvim
+if [ ! -d $HOME/.config/nvim ]; then
+    git clone git@github.com:marciomazza/neovim_config.git $HOME/.config/nvim
 fi
